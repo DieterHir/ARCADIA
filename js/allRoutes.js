d1e00c5334f390dad94ecd069151423ad4edbe4e
Dieter <dieter59136@live.fr>
2024-07-09 17:33:37 +0200

    main pages added and router updated

--- js/allRoutes.js
+++ js/allRoutes.js
@@ -2,6 +2,10 @@ import Route from "./Route.js";
 
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/services", "Nos Services", "/pages/services.html"),
+    new Route("/habitats", "Les Habitats", "/pages/habitats.html"),
+    new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/login", "Connexion", "/pages/login.html"),
 ];
 
 export const websiteName = "Arcadia";
